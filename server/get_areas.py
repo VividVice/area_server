286c06fb884b2d97babf588e317b2d2996dcbdf5
VividVice <vasiliy.novikov@epitech.eu>
2023-11-06 19:48:30 +0100

    update area

--- server/get_areas.py
+++ server/get_areas.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
 from modules.database.DB import DataBaseOpps
 from modules.config.config import Config
+from modules.utils.jsonToken import UnpackToken
 
 app = Config().GetApp()
 
 @app.route('/get_subscribed_areas', methods=['GET'])
 def get_subscribed_areas():
     # Get the username from the request parameters or headers, depending on your use case
-    username = request.args.get('username')
-
-    if username:
-        user = DataBaseOpps.GetUser(username)
+    token = request.headers.get('Authorization')
+    if token and token.startswith('Bearer '):
+        payload = UnpackToken(token, False)
+    if payload:
+        user = DataBaseOpps.GetUser(payload["username"])
         if user:
             subscribed_areas = [service for service, value in user.user_services.items() if value]
             return jsonify(subscribed_areas)
