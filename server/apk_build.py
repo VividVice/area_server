623fbb389c95452bd37ef68b4ff723a04f822537
VividVice <vasiliy.novikov@epitech.eu>
2023-11-06 18:57:06 +0100

    stufff

--- server/apk_build.py
+++ server/apk_build.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_from_directory
 from modules.config.config import Config
+import os
+from flask import current_app
 # ... your existing code ...
 
 app = Config().GetApp()
 
 @app.route('/client.apk', methods=['GET'])
 def download_apk():
-    return send_from_directory('./build/', 'client.apk')
+    uploads = os.path.join(current_app.root_path, 'build')
+    return send_from_directory(uploads, 'client.apk')
 
