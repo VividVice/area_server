3e22a35b5ff7320e29ab35338712323edd7cae03
Lamouette1 <charles.fassel-ashley@epitech.eu>
2023-11-06 18:42:12 +0100

    Add useEffect hook to navigate to Dashboard after GithubOauth and fix redirect url bug in GithubOauth.js

--- mobile/AreaCraft/GoToGithub.js
+++ mobile/AreaCraft/GoToGithub.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GithubOauth from './Logics/GithubOauth';
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 export default function Gotogith() {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(null);
+  const navigation = useNavigation();
 
-  return (
-    <GithubOauth SetTokenSession={setToken} />
-  )
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+      navigation.navigate('Dashboard', { token: token });
+    }
+  }, [token, navigation]);
+
+  if (!token) {
+    return <GithubOauth SetTokenSession={setToken} />;
+  }
+
+  return null;
 }
