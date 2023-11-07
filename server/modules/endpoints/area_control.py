d8fc127748662b3ae03473d762a7c0086b98403c
Eliyan <eliyan.dochev@epitech.eu>
2023-11-07 18:44:59 +0100

    [CHHANGED] the type of arg from int to string

--- server/modules/endpoints/area_control.py
+++ server/modules/endpoints/area_control.py
@@ -9,13 +9,28 @@ app = Config().GetApp()
 
 class Area_Control(Resource):
     def get(self):
-        # Get the username from the request parameters or headers, depending on your use case
+        """Get all the areas for a specific user.
+           return: a list of all the areas for a specific user in the following format:
+              [
+                {
+                     "action_service_name": "trello",
+                     "action": "createBoard",
+                     "reaction_name": "createCard",
+                     "reaction_service_name": "trello",
+                     "id": 0,
+                     "params_reaction": {
+                          "list_id": "5f9b7b4b7b7b7b7b7b7b7b7b",
+                          "card_name": "test card"
+                     }
+                }
+              ]
+        """
         token = request.headers.get('Authorization')
         payload = UnpackToken(token, True)
         if payload:
             user = DB.GetUser(payload["username"])
             if user:
-                # extract from user.user_services the subscribed areas 
+                # extract from user.user_services the subscribed areas
                 # {"service_name": "trello", "Areas": [{"action": "create_card", "subbed_reactions": [{"service_name": "callr", "function_name": "make_call", "params": {"phone_number": "+972546777777"}}]}]}
                 # so bacissaly extract the Areas from user.user_services["service_name"] but keep the key service_name
                 # also for each reaction subbed for each action add an id that corresponds to the index of the reaction in the subbed_reactions array
@@ -42,20 +57,31 @@ class Area_Control(Resource):
             {"error": "Username not provided"}, 403
 
     def post(self):
+        """
+            Delete an area for a specific user.
+            send the following json:
+            {
+                "action_service_name": "trello",
+                "action": "createBoard",
+                "id": 0 # the id of the reaction in the subbed_reactions array
+            }
+            return: a message indicating if the area was deleted or not
+        """
         token = request.headers.get('Authorization')
         payload = UnpackToken(token, True)
         parser = reqparse.RequestParser()
         parser.add_argument('action_service_name', type=str, required=True)
         parser.add_argument('action', type=str, required=True)
-        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('id', type=str, required=True)
         args = parser.parse_args()
+        id = int(args["id"])
         if payload:
             user = DB.GetUser(payload["username"])
             if user :
                 try :
                     for area in user.user_services[args["action_service_name"]]["Areas"]:
                         if area["action"] == args["action"]:
-                            area["subbed_reactions"].pop(args["id"])
+                            area["subbed_reactions"].pop(id)
                             # check if is it is the last reaction if so execute delete webhook
                             if len(area["subbed_reactions"]) == 0:
                                 # delete webhook
